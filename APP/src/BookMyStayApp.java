0b139943e6d1faf44533f6f0efdd9128ff7cf58c
ms8137-web <ms8137@srmist.edu.in>
2026-03-13 09:36:56 +0530

    MANAN Added feature/UC1-WelcomePage

---
+++ APP/src/BookMyStayApp.java
@@ -0,0 +1,23 @@
+
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        // Application Entry
+        System.out.println("=================================");
+        System.out.println("        Book My Stay App         ");
+        System.out.println("=================================");
+
+        // Welcome Message
+        System.out.println("Welcome to Book My Stay Application");
+
+        // Application Details
+        System.out.println("Version : 1.0");
+        System.out.println("This application helps users book hotels easily.");
+
+        System.out.println("---------------------------------");
+        System.out.println("Application Started Successfully");
+        System.out.println("---------------------------------");
+
+    }
+}
